46ab86d9802c623e8d674304c5803d9854d4da52
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-10-09 20:49:22 +0300

    rightbar progress

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,23 +3,30 @@ import classNames from "classnames";
 import { createElement } from "react";
 import PropTypes from "prop-types";
 
-export default function Button({ size, variant, children}) { //children ile tek sefer yaz sonrasinda istedigin yerde kullan.
-    return createElement('button', {
+export default function Button({ as, size, variant, className, children , ... props}) { //children ile tek sefer yaz sonrasinda istedigin yerde kullan.
+    return createElement(as, {
         className: classNames( "font-bold flex items-center justify-center rounded-full transition-colors", {
             "px-4 h-8 text-sm" : size === 'small',
             "px-4 h-9" : size === 'normal',
             "px-4 text-[17px] h-[52px] w-full" : size === 'large',
             "bg-[#1d9bf0] hover:bg-[#1a8cd8] text-white": variant === 'primary',
             "bg-[#eff3f4] hover:bg-[#d7dbdc] text-black": variant === 'white',
-        })
+            "border border-[#b4b4b4] hover:border-[#67070f] hover-bg-[#f4212e1a] hover:text-[#f4212e]": variant === 'white-outline',
+            [className]: true
+        }),
+        ... props
     }, children)
 }
 
 Button.propTypes = {
+    as: PropTypes.any,
     size: PropTypes.oneOf(['small','normal','large']),
-    variant: PropTypes.oneOf(['primary','white'])
+    variant: PropTypes.oneOf(['primary','white','white-outline']),
+    props: PropTypes.object,
+    className: PropTypes.string
 }
 Button.defaultProps = {
+    as:'button',
     size: 'normal',
     variant:'primary'
 }
--- src/layouts/main/rightbar/footer/index.jsx
+++ src/layouts/main/rightbar/footer/index.jsx
@@ -1,11 +1,75 @@
 import { Link } from "react-router-dom";
+import { Popover } from "@headlessui/react";
 
 export default function Footer() {
-    return (
-        <footer className="mb-4 px-4 flex flex-wrap gap-2">
-            <Link to="/" className="text-[#71767b] leading-4 text-[13px] hover:underline">
-                Hizmet Şartları
+  return (
+    <footer className="mb-4 px-4 flex flex-wrap gap-2">
+      <Link
+        to="/"
+        className="text-[#71767b] leading-4 text-[13px] hover:underline"
+      >
+        Hizmet Şartları
+      </Link>
+      <Link
+        to="/"
+        className="text-[#71767b] leading-4 text-[13px] hover:underline"
+      >
+        Gizlilik Politikası
+      </Link>
+      <Link
+        to="/"
+        className="text-[#71767b] leading-4 text-[13px] hover:underline"
+      >
+        Çerez Politikası
+      </Link>
+      <Link
+        to="/"
+        className="text-[#71767b] leading-4 text-[13px] hover:underline"
+      >
+        İmprint
+      </Link>
+      <Link
+        to="/"
+        className="text-[#71767b] leading-4 text-[13px] hover:underline"
+      >
+        Erişilebilirlik
+      </Link>
+      <Link
+        to="/"
+        className="text-[#71767b] leading-4 text-[13px] hover:underline"
+      >
+        Reklam Bilgisi
+      </Link>
+      <div className="w-full flex gap-2">
+        <Popover className="relative outline-none leading-4 inline-flex ">
+          <Popover.Button className="text-[#71767b] leading-4 text-[13px] hover:underline inline-flex items-center">
+            Daha fazla
+            <svg viewBox="0 0 24 24" className="h-[1em] px-0.5">
+              <path
+                fill="currentColor"
+                d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
+              />
+            </svg>
+          </Popover.Button>
+          <Popover.Panel className="w-[150px] grid absolute bottom-0 right-0 max-w-[384px] bg-black shadow-box rounded-xl overflow-hidden">
+            <Link className="py-3 px-4 text-white text-[15px] hover:bg-[#16181c] leading-5 font-bold">
+              Hakkında
             </Link>
-        </footer>
-    )
+            <Link className="py-3 px-4 text-white text-[15px] hover:bg-[#16181c] leading-5 font-bold">
+              Durum
+            </Link>
+            <Link className="py-3 px-4 text-white text-[15px] hover:bg-[#16181c] leading-5 font-bold">
+              İşletmeler İçin X
+            </Link>
+            <Link className="py-3 px-4 text-white text-[15px] hover:bg-[#16181c] leading-5 font-bold">
+              Geliştiriciler
+            </Link>
+          </Popover.Panel>
+        </Popover>
+        <p className="text-[#71767b] leading-4 text-[13px]">
+          &copy; 2023 MustafaTutus
+        </p>
+      </div>
+    </footer>
+  );
 }
